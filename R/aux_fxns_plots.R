0732cf5f6eaf6927827fae18b07b9b3cd53f9e4b
giuliaottino <botgiuc@gmail.com>
2026-08-19 23:10:37 -0300

    Fix cross-platform text normalization

--- R/aux_fxns_plots.R
+++ R/aux_fxns_plots.R
@@ -137,10 +137,80 @@
 #' @noRd
 .clean_chr <- function(x) {
   x <- as.character(x)
-  x <- iconv(x, to = "ASCII//TRANSLIT", sub = "")
   x[is.na(x)] <- ""
-  x <- gsub(intToUtf8(0x00A0), " ", x, fixed = TRUE)
-  x <- gsub("\\s+", " ", x)
+
+  # Normalize non-breaking spaces before any other processing
+  x <- gsub(
+    intToUtf8(0x00A0),
+    " ",
+    x,
+    fixed = TRUE
+  )
+
+  # Multi-character Latin substitutions
+  replacements <- c(
+    "\u00c6" = "AE",
+    "\u00e6" = "ae",
+    "\u0152" = "OE",
+    "\u0153" = "oe",
+    "\u00df" = "ss",
+    "\u00d8" = "O",
+    "\u00f8" = "o",
+    "\u0141" = "L",
+    "\u0142" = "l"
+  )
+
+  for (from in names(replacements)) {
+    x <- gsub(
+      from,
+      replacements[[from]],
+      x,
+      fixed = TRUE
+    )
+  }
+
+  # One-to-one Latin accent transliteration
+  x <- chartr(
+    paste0(
+      "\u00c0\u00c1\u00c2\u00c3\u00c4\u00c5",
+      "\u00e0\u00e1\u00e2\u00e3\u00e4\u00e5",
+      "\u00c8\u00c9\u00ca\u00cb",
+      "\u00e8\u00e9\u00ea\u00eb",
+      "\u00cc\u00cd\u00ce\u00cf",
+      "\u00ec\u00ed\u00ee\u00ef",
+      "\u00d2\u00d3\u00d4\u00d5\u00d6",
+      "\u00f2\u00f3\u00f4\u00f5\u00f6",
+      "\u00d9\u00da\u00db\u00dc",
+      "\u00f9\u00fa\u00fb\u00fc",
+      "\u00c7\u00e7",
+      "\u00d1\u00f1",
+      "\u00dd\u0178\u00fd\u00ff"
+    ),
+    paste0(
+      "AAAAAA",
+      "aaaaaa",
+      "EEEE",
+      "eeee",
+      "IIII",
+      "iiii",
+      "OOOOO",
+      "ooooo",
+      "UUUU",
+      "uuuu",
+      "Cc",
+      "Nn",
+      "YYyy"
+    ),
+    x
+  )
+
+  # Normalize whitespace
+  x <- gsub(
+    "[[:space:]]+",
+    " ",
+    x
+  )
+
   trimws(x)
 }
 
